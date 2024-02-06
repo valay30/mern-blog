2adf8293db58c588c07942ac4c62e66c5f525c5b
valay30 <patelvalay308@gmail.com>
2024-02-06 19:21:08 +0530

    complete user image upload

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -19,7 +19,7 @@ export default function DashSidebar() {
       <Sidebar.Items>
         <Sidebar.ItemGroup >
           <Link to='/dashboard?tab=profile'>
-          <Sidebar.Item active={tab == 'profile'} icon={HiUser} label={'User'} lableColor='dark'>
+          <Sidebar.Item active={tab == 'profile'} icon={HiUser} label={'User'} lableColor='dark' as='div'>
               Profile
             </Sidebar.Item>
             </Link>
