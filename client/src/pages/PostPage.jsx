c02cd15cfb7624172e725ae8526d0b7395725ab0
valay30 <patelvalay308@gmail.com>
2024-02-08 13:20:20 +0530

    add recent article section to the post page

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import CallToAction from '../components/CallToAction';
 import CommentSection from '../components/CommentSection';
-// import PostCard from '../components/PostCard';
+import PostCard from '../components/PostCard';
 
 export default function PostPage() {
   const { postSlug } = useParams();
@@ -90,13 +90,13 @@ export default function PostPage() {
       </div>
       <CommentSection postId={post._id} />
 
-      {/* <div className='flex flex-col justify-center items-center mb-5'>
+      <div className='flex flex-col justify-center items-center mb-5'>
         <h1 className='text-xl mt-5'>Recent articles</h1>
         <div className='flex flex-wrap gap-5 mt-5 justify-center'>
           {recentPosts &&
             recentPosts.map((post) => <PostCard key={post._id} post={post} />)}
         </div>
-      </div> */}
+      </div>
     </main>
   );
 }
